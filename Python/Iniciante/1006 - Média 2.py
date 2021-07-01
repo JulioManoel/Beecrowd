161fb77eac94c162db6a1a02e77e83f239fa28cb
Julio Cesar Bonow Manoel <juliobonow@gmail.com>
2021-07-01 10:32:28 -0300

    Atualização

--- Python/Iniciante/1006 - Média 2.py
+++ Python/Iniciante/1006 - Média 2.py
@@ -1,11 +1,5 @@
-#include <stdio.h>
- 
-int main() {
-    double a,b,c,media;
-    scanf("%lf",&a);
-    scanf("%lf",&b);
-    scanf("%lf",&c);
-    media = ((a*2)+(b*3)+(c*5))/10;
-    printf("MEDIA = %.1lf\n",media);
-    return 0;
-}
+a = float(input())
+b = float(input())
+c = float(input())
+media = ((a*2)+(b*3)+(c*5))/10
+print("MEDIA = {}\n".format(media))
