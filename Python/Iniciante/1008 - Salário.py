b229192748dcf65b39d5e8bfe2d6ee2436a67f3f
Julio Cesar Bonow Manoel <juliobonow@gmail.com>
2021-07-11 21:16:12 -0300

    Atualização

--- Python/Iniciante/1008 - Salário.py
+++ Python/Iniciante/1008 - Salário.py
@@ -1,4 +1,4 @@
 fun = int(input())
 horas = int(input())
 valor = float(input())
-print("NUMBER = {}\nSALARY = U$ {}\n".format(fun,round(valor*horas,2)))
+print("NUMBER = {}\nSALARY = U$ {}\n".format(fun,round(valor*horas,2)))sw
