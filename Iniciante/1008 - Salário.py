4e08ce5a7e6fdf71bdd0afded04355dd507ef65c
Julio Cesar Bonow Manoel <84098213+JulioManoel@users.noreply.github.com>
2023-09-15 09:22:11 -0300

    Delete Iniciante directory

--- Iniciante/1008 - Salário.py
+++
@@ -1,4 +0,0 @@
-fun = int(input())
-horas = int(input())
-valor = float(input())
-print("NUMBER = {}\nSALARY = U$ {}\n".format(fun,round(valor*horas,2)))
--- Iniciante/1009 - Salário Bônus.py
+++
@@ -1,5 +0,0 @@
-nome = input()
-salario = float(input())
-comissao = float(input())
-salario+=(comissao*0.15)
-print("TOTAL = R$ {}\n".format(salario))
